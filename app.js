70e2a814b57c3006148a23885e0836e49b3878f6
Sudheesh Singanamalla <sudheesh@cs.washington.edu>
2019-10-25 11:58:51 -0700

    Adds package.json and lock along with .gitignore, change binary exec
    Signed-off-by: Sudheesh Singanamalla <sudheesh@cs.washington.edu>

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var nunjucks = require( 'nunjucks' ) ;
 var app = express() ;
 
 var PATH_TO_TEMPLATES = '.' ;
-nunjucks.configure( PATH_TO_TEMPLATES, {
+nunjucks.configure(PATH_TO_TEMPLATES, {
     autoescape: true,
     express: app
-} ) ;
+});
 
 app.get( '/home.html', function( req, res ) {
     return res.render( 'index.html' ) ;
-} ) ;
-app.listen( 3000 ) ;
+});
+
+module.exports = app;
