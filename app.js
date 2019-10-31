db8c6d0a5f22cf99fde09eb75aadb501e3f32af4
yer4 <yer4@cs.washington.edu>
2019-10-31 06:20:02 -0700

    begin adding the engine templates

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var express = require('express');
 var nunjucks = require('nunjucks');
 var app = express();
 
+//js/data contains the static gauge javascript files
+//css folder contains files
+app.use('*/js',express.static(path.join(__dirname, 'js/data/')));
+app.use('*/css',express.static(path.join(__dirname, 'css')));
+//could try these options too if doesn't work
+//app.use(express.static('js/data/')); app.use(express.static('css));
+//app.use('*/css',express.static('css'));
+
 var PATH_TO_TEMPLATES = '.' ;
 nunjucks.configure(PATH_TO_TEMPLATES, {
     autoescape: true,
@@ -12,9 +20,8 @@ app.get('/home.html', function(req, res) {
     return res.render('index.html') ;
 });
 
-//use static files from our project
-app.use('*/js',express.static(path.join(__dirname, 'js/data/')));
-app.use('*/css',express.static(path.join(__dirname, '/css')));
+app.set('views', './views'); //directory where template files will be located
 
+app.set('view engine', 'pug'); //use Pug template engine
 
 module.exports = app;
