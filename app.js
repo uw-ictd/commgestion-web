4ccb4f0cf6a7f6827d9377a6be29a9be16d800dd
frankieo <frankieo@cs.washington.edu>
2019-10-31 15:02:02 -0700

    nunjuck template files created with some routes - need to be fleshed out

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 app.use(express.static('js/data/'));
 app.use(express.static('css'));
 
-var PATH_TO_TEMPLATES = '.' ;
+var PATH_TO_TEMPLATES = './views' ;
 nunjucks.configure(PATH_TO_TEMPLATES, {
     autoescape: true,
     express: app
@@ -17,12 +17,29 @@ app.get('/home.html', function(req, res) {
     return res.render('index.html') ;
 });
 
-app.set('views', './views'); //directory where template files will be located
+//app.set('views', './views'); //directory where template files will be located
 
-app.set('view engine', 'pug'); //using Pug type template engine
+//app.set('view engine', 'pug'); //using Pug type template engine
+app.set('view engine', 'njk');
 
 app.get('/', function (req, res) { //route to index.pug file
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  return res.render('_layout.njk');
 })
+app.get('/stats', function(req, res) {
+    return res.render('overall.njk');
+})
+
+app.get('/usuario', function(req, res){
+    return res.render('usuario.njk');
+})
+
+app.get('/profile', function(req, res){
+    return res.render('profile.njk')
+})
+
+app.get('/public', function(req, res){
+    return res.render('public_info.njk')
+})
+
 
 module.exports = app;
