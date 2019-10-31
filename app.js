1b98e8cd910a0b9d41fa13bb9dd19bbe0e423c0e
yer4 <yer4@cs.washington.edu>
2019-10-31 06:41:10 -0700

    created empty views folder for the templates. Added nodemon script in package.json. Added static files in the app.js

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ var app = express();
 
 //js/data contains the static gauge javascript files
 //css folder contains files
-app.use('*/js',express.static(path.join(__dirname, 'js/data/')));
-app.use('*/css',express.static(path.join(__dirname, 'css')));
-//could try these options too if doesn't work
-//app.use(express.static('js/data/')); app.use(express.static('css));
-//app.use('*/css',express.static('css'));
+app.use(express.static('js/data/'));
+app.use(express.static('css'));
 
 var PATH_TO_TEMPLATES = '.' ;
 nunjucks.configure(PATH_TO_TEMPLATES, {
@@ -22,6 +19,10 @@ app.get('/home.html', function(req, res) {
 
 app.set('views', './views'); //directory where template files will be located
 
-app.set('view engine', 'pug'); //use Pug template engine
+app.set('view engine', 'pug'); //using Pug type template engine
+
+app.get('/', function (req, res) { //route to index.pug file
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
 
 module.exports = app;
