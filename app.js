51bae76c6a42f9fd3f73770f66c7627e392efbb8
yer4 <yer4@cs.washington.edu>
2019-10-31 06:01:42 -0700

    Specified static static files for express (unsure if it is correct)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express = require( 'express' ) ;
-var nunjucks = require( 'nunjucks' ) ;
-var app = express() ;
+var express = require('express');
+var nunjucks = require('nunjucks');
+var app = express();
 
 var PATH_TO_TEMPLATES = '.' ;
 nunjucks.configure(PATH_TO_TEMPLATES, {
@@ -8,8 +8,13 @@ nunjucks.configure(PATH_TO_TEMPLATES, {
     express: app
 });
 
-app.get( '/home.html', function( req, res ) {
-    return res.render( 'index.html' ) ;
+app.get('/home.html', function(req, res) {
+    return res.render('index.html') ;
 });
+
+//use static files from our project
+app.use('*/js',express.static(path.join(__dirname, 'js/data/')));
+app.use('*/css',express.static(path.join(__dirname, '/css')));
+
 
 module.exports = app;
