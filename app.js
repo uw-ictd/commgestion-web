f35747b177be73683cf498b1fe40d642aff0f3a3
yer4 <yer4@cs.washington.edu>
2019-10-31 17:23:35 -0700

    Fixes #1 implements bare bone nunjucks

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
+var path = require('path');
 var express = require('express');
 var nunjucks = require('nunjucks');
 var app = express();
 
-//js/data contains the static gauge javascript files
-//css folder contains files
-app.use(express.static('js/data/'));
-app.use(express.static('css'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 var PATH_TO_TEMPLATES = './views' ;
 nunjucks.configure(PATH_TO_TEMPLATES, {
@@ -13,16 +11,11 @@ nunjucks.configure(PATH_TO_TEMPLATES, {
     express: app
 });
 
-app.get('/home.html', function(req, res) {
-    return res.render('index.html') ;
-});
-
 //app.set('views', './views'); //directory where template files will be located
 
-//app.set('view engine', 'pug'); //using Pug type template engine
 app.set('view engine', 'njk');
 
-app.get('/', function (req, res) { //route to index.pug file
+app.get('/', function (req, res) {
   return res.render('_layout.njk');
 })
 app.get('/stats', function(req, res) {
