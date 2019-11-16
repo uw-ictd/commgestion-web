44c65aacfe11cf7c6cfcf406df6b7f8a9ebbe19a
Sudheesh Singanamalla <sudheesh@cs.washington.edu>
2019-11-15 17:22:32 -0800

    Avoid recreating the user entry in the database on start
    Signed-off-by: Sudheesh Singanamalla <sudheesh@cs.washington.edu>

--- temp-test/applicationUser.js
+++ temp-test/applicationUser.js
@@ -5,25 +5,33 @@ let User = userFactory(db.sequelize, db.Sequelize.DataTypes);
 
 function test() {
     db.sequelize.sync().then(function() {
-        return User.create({
-            username: 'fiddle',
-            phoneNumber: '+1(456)789-1034',
-            displayName: 'Superstar',
-            imsi: '1234567890',
-            guti: '_',
-            isLocal: true,
-            role: 'user',
-            connectivityStatus: 'online',
-            lastTimeOnline: Date.now(),
-            rateLimitKbps: -1,
-            salt: 'fish',
-            passwordSha256: 'undefined',
+        return User.findOrCreate({
+            where: {
+                username: 'fiddle'
+            },
+            defaults: {
+                phoneNumber: '+1(456)789-1034',
+                displayName: 'Superstar',
+                imsi: '1234567890',
+                guti: '_',
+                isLocal: true,
+                role: 'user',
+                connectivityStatus: 'online',
+                lastTimeOnline: Date.now(),
+                rateLimitKbps: -1,
+                salt: 'fish',
+                passwordSha256: 'undefined'
+            }
+        }).then(([createdUser, created]) => {
+            if (!created) {
+                console.error("User already exists. Avoiding the creation of a duplicate entry");
+            } else {
+                console.log(createdUser.get({
+                    plain: true
+                }));
+            }
         });
-    }).then(function(createdUser) {
-        console.log(createdUser.get({
-            plain: true
-        }));
     });
 }
 
-module.exports = test
+module.exports = test;
