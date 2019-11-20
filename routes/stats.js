37adf1ed432165c20f87b5c5252bf993e9dbe06f
yer4 <yer4@cs.washington.edu>
2019-11-19 20:10:45 -0800

    started on fixing redundancy for stats njk

--- routes/stats.js
+++ routes/stats.js
@@ -5,7 +5,12 @@ let constants = require('../constants');
 /* GET stats page. */
 router.get('/',
     function(req, res, next) {
-        res.render(constants.STATISTICS_PAGE, {title: 'Express', user: req.user});
+        res.render(constants.STATISTICS_PAGE, {title: 'Express', user: req.user,
+        rows: {
+            top:["thru-vs-time", "thru-by-app"],
+            bottom:["graph-3", "graph-4"]
+        }
+        });
     });
 
 module.exports = router;
