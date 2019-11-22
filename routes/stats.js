4c772c2414b650db7e711487780217390072d026
frankieo <frankieo@cs.washington.edu>
2019-11-21 21:47:08 -0800

    nunjucks changes from work session

--- routes/stats.js
+++ routes/stats.js
@@ -5,11 +5,11 @@ let constants = require('../constants');
 /* GET stats page. */
 router.get('/',
     function(req, res, next) {
-        res.render(constants.STATISTICS_PAGE, {title: 'Express', user: req.user,
-        rows: {
-            top:["thru-vs-time", "thru-by-app"],
-            bottom:["graph-3", "graph-4"]
-        }
+        let rowBuilder = [
+                ["thru-vs-time", "thru-by-app"],
+                ["graph-3", "graph-4"]
+        ];
+        res.render(constants.STATISTICS_PAGE, {title: 'Express', user: req.user, rows: rowBuilder
         });
     });
 
