80fcd0322921ffe3a8ac1866cd316af94d8016db
Matt Johnson <matt9j@cs.washington.edu>
2020-01-16 14:26:29 -0800

    Initial plumbing

--- commgestion/urls.py
+++ commgestion/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('/', include('web.urls')),
+    path('', include('web.urls')),
     path('admin/', admin.site.urls)
-
-]
+] + static(settings.STATIC_URL)
--- web/urls.py
+++ web/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
     path('', views.home, name='home')
 ]
