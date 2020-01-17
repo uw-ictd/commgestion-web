d832b4923dc5887fad6715b748a2367327adafff
Matt Johnson <matt9j@cs.washington.edu>
2020-01-16 18:06:04 -0800

    Add test template context

--- web/views.py
+++ web/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 
 def net_stats(request):
-    return render(request, 'stats.html')
+    context = {
+        'totalUsers': 10,
+    }
+    return render(request, 'stats.html', context=context)
 
 
 def public_info(request):
     return render(request, 'public_info.html')
 
+
 def profile(request):
     return render(request, 'profile.html')
+
 
 def usuario(request):
     return render(request, 'usuario_pie.html')
