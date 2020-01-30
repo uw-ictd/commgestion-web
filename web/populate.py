586d7beca4ccb1fd70d5df5a1285b53fbcf763e8
yer4 <yer4@cs.washington.edu>
2020-01-30 13:51:22 -0800

    finished the subscribers functions

--- web/populate.py
+++ web/populate.py
@@ -8,45 +8,44 @@ from django.utils import timezone
 
 # add more applications
 def add_applications():
-        hosts = ['https://google.com/', 'https://facebook.com', 'https://whatsapp.com', 'https://santainesapp.mx']
-        for host_name in hosts:
-            Application.objects.create(
-                host=host_name,
-                throughput=1000 * random.random(),
-                timestamp=timezone.now(),
-            )
+    hosts = ['https://google.com/', 'https://facebook.com', 'https://whatsapp.com', 'https://santainesapp.mx']
+    for host_name in hosts:
+        Application.objects.create(
+            host=host_name,
+            throughput=1000 * random.random(),
+            timestamp=timezone.now())
 
 #  add host mapping as well as user defined hosts
 def add_hostmappings():
-        UserDefinedHost.objects.all().delete()
-        captured_fb = list(['fb.cdn.com', 'dragon.cdn.com'])
-        captured_google = list(['google.com','googlehost1.com', 'googlehost2.com', 'googlehost3'])
-        captured_youtube = list(['youtube.com', 'you1', 'youhost2'])
-        captured_wikipedia = list(['wikipedia.org', 'wiki', 'wik', 'wik2'])
+    UserDefinedHost.objects.all().delete()
+    captured_fb = list(['fb.cdn.com', 'dragon.cdn.com'])
+    captured_google = list(['google.com','googlehost1.com', 'googlehost2.com', 'googlehost3'])
+    captured_youtube = list(['youtube.com', 'you1', 'youhost2'])
+    captured_wikipedia = list(['wikipedia.org', 'wiki', 'wik', 'wik2'])
 
-
-        UserDefinedHost.objects.create(name='google.com')
-        udh_google = UserDefinedHost.objects.get(name='google.com')
-        UserDefinedHost.objects.create(name='facebook.com')
-        udh_fb = UserDefinedHost.objects.get(name='facebook.com')
-        UserDefinedHost.objects.create(name='youtube.com')
-        udh_youtube = UserDefinedHost.objects.get(name='youtube.com')
-        UserDefinedHost.objects.create(name='wikipedia.org')
-        udh_wiki = UserDefinedHost.objects.get(name='wikipedia.org')
-
+    UserDefinedHost.objects.create(name='google.com')
+    udh_google = UserDefinedHost.objects.get(name='google.com')
+    UserDefinedHost.objects.create(name='facebook.com')
+    udh_fb = UserDefinedHost.objects.get(name='facebook.com')
+    UserDefinedHost.objects.create(name='youtube.com')
+    udh_youtube = UserDefinedHost.objects.get(name='youtube.com')
+    UserDefinedHost.objects.create(name='wikipedia.org')
+    udh_wiki = UserDefinedHost.objects.get(name='wikipedia.org')
         
-        udh_list = [(udh_fb, captured_fb),  (udh_google, captured_google),
-        (udh_youtube, captured_youtube), (udh_wiki, captured_wikipedia)]
+    udh_list = [(udh_fb, captured_fb),  (udh_google, captured_google),
+    (udh_youtube, captured_youtube), (udh_wiki, captured_wikipedia)]
 
-        for hm in udh_list:
-            HostMapping.objects.create(host=hm[0], captured_host=hm[1])
-        
-        print(UserDefinedHost.objects.all())
+    for hm in udh_list:
+        HostMapping.objects.create(host=hm[0], captured_host=hm[1])
+
+    print(UserDefinedHost.objects.all())
 """ 
 #add subscribers, which requires fake "User" info like email phone imsi
 def add_subscribers():
  """
 def add_subscribers():
+    Subscriber.objects.all().delete()
+    User.objects.all().delete()
     IMSI_VALUES = ['1234567890', '1234567891','1234567892','1234567893','1234567894']
     EMAIL_VALUES = ['person@ccellular.network','person1@ccellular.network','person2@ccellular.network',
     'person3@ccellular.network','person4@ccellular.network']
@@ -56,19 +55,19 @@ def add_subscribers():
     #make users
     for i in range(len(IMSI_VALUES)):
         User.objects.create_user(username=IMSI_VALUES[i], email = EMAIL_VALUES[i], password=PASSWORDS[i])
-        created_user = User.objects.get(username=IMSI_VALUE)
+        created_user = User.objects.get(username=IMSI_VALUES[i])
         created_users.append(created_user)
         INSERT_TIMESTAMP = timezone.now()
     
     #make subscribers
     for i in range(len(created_users)):
-        user = created_user[i]
+        user = created_users[i]
         Subscriber.objects.create(
             user=user,
             phonenumber=IMSI_VALUES[i],
             display_name=EMAIL_VALUES[i].split('@')[0],
             imsi=IMSI_VALUES[i],
-            guti=GUTI_VALUE[i],
+            guti=GUTI_VALUES[i],
             is_local=True,
             role=Subscriber.Role.USER_ROLE,
             connectivity_status=Subscriber.ConnectionStatus.ONLINE,
@@ -76,20 +75,14 @@ def add_subscribers():
             rate_limit_kbps=100,
         )
         
-        #add some usage for this subscirber
-        created_subscriber = Subscriber.objects.get(phonenumber=IMSI_VALUE[i])
+        #add some usage for this subscriber
+        created_subscriber = Subscriber.objects.get(phonenumber=IMSI_VALUES[i])
         Usage.objects.create(user=created_subscriber, throughput=50*random.random(), timestamp=timezone.now())
 
-    subscriber_list=list()
-    i = 1
-    while i <= 10:
-        list.append(
-            Subscriber.objects.create(
-            Subscriber.Role= 1,
-            Subscriber.connectivity_status=1,
-            Subscriber.ROLE_CHOICES=(Role.ADMIN_ROLE, u'admin'),
-            Subscriber.CONN_CHOICES =(ConnectionStatus.ONLINE, u'online')
-        )))
+def get_subscribers():
+    total = Subscriber.objects.all()
+    return total
+
 """ class SubscriberModelTest(TestCase):
     def test_subscriber_creation(self):
         #insert user into database
