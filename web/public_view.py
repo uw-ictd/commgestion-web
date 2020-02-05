af846e8cf5b480fafc0f0f87722af3c65d3357f8
frankieo <frankieo@cs.washington.edu>
2020-02-05 14:08:16 -0800

    setting up data for public gauge (wip)

--- web/public_view.py
+++ web/public_view.py
@@ -1,5 +1,21 @@
 import json
+import random
+from web.models import Subscriber, Usage
+from django.utils import timezone
+from django.db.models import Sum
 
 def generate_test_data():
+    
+    #make some "current usage" data (need this to run every time so that we can have some fresh numbers. 
+    k = 5
+    usr = Subscriber.objects.get(phonenumber='123456789' + str(random.randint(0, 5)))
+    time = timezone.now()
+    while k >= 0 :
+        Usage.objects.create(user=usr, throughput=50*random.random(), timestamp=time)
+        k-=1
+    
+    #query for the current usage:
+    qs_agg = Usage.objects.filter(timestamp=time).annotate(thru = Sum('throughput'))
+    print(qs_agg)
     data =[4]
     return {"data": data}
