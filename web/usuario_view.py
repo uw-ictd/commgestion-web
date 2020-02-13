068b5632b8bdae57dafcaf9dc5c9b442eb11cdff
yer4 <yer4@uw.edu>
2020-02-13 14:25:01 -0800

    fixed the usuario_view to query total throughput sums for each distinct user

--- web/usuario_view.py
+++ web/usuario_view.py
@@ -1,36 +1,22 @@
 import json
 
+from django.db.models import Sum
+
 from web.models import Subscriber, Usage
+
+def lookup_user(user_id):
+    subscriber = Subscriber.objects.get(pk=user_id)
+    return subscriber.display_name
 
 
 def generate_test_data():
-    usageData = Usage.objects.all()
+    usageData = Usage.objects.values("user").annotate(Sum("throughput"))
     data = []
     for usage in usageData:
         data.append(
             {
-                'name': usage.get_username(),
-                'y': usage.throughput,
+                'name': lookup_user(usage['user']),
+                'y': usage['throughput__sum'],
             }
         )
-    # data = [
-    #     {
-    #         'name': 'Usuario 1',
-    #         'y': 61.41,
-    #     }, {
-    #         'name': 'Usuario 2',
-    #         'y': 11.84
-    #     }, {
-    #         'name': 'Usuario 3',
-    #         'y': 10.85
-    #     }, {
-    #         'name': 'Usuario 4',
-    #         'y': 4.67
-    #     }, {
-    #         'name': 'Usuario 5',
-    #         'y': 4.18
-    #     }, {
-    #         'name': 'Other',
-    #         'y': 7.05
-    #     }]
     return {'data': json.dumps(data)}
