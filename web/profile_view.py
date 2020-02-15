dd8ec9464bba5763060b9016cb73c6fbeb98304c
frankieo <frankieo@cs.washington.edu>
2020-02-14 17:59:21 -0800

    no frills data table in place

--- web/profile_view.py
+++ web/profile_view.py
@@ -3,5 +3,4 @@ from django.core import serializers
 
 def generate_table():
     all_subscribers = Subscriber.objects.all()
-    json = serializers.serialize('json', all_subscribers)
-    return {"data": json}
+    return {"data": all_subscribers}
