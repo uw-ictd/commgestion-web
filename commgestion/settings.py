c4b1646943c7044418c93a561c460954434952bb
Matt Johnson <matt9j@cs.washington.edu>
2020-03-09 22:02:18 -0700

    Re-home orphan comment

--- commgestion/settings.py
+++ commgestion/settings.py
@@ -14,9 +14,9 @@ import os
 
 from django.utils.translation import ugettext_lazy as _
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from commgestion.config import DATABASE_CONFIG
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
