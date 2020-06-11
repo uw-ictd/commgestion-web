e3bb775dc0bf5c2d21b276802f4cc317f65f0155
Matt Johnson <matt9j@cs.washington.edu>
2020-06-11 14:18:17 -0700

    [management] Add a `populatedb` management command
    Use `python manage.py populatedb to execute a database population!
    This should be easier than opening a shell and manually importing the
    populate file. populatedb intentionally does not work when the app is
    not in debug mode, as a check to prevent overriding production data!

---
+++ web/management/commands/populatedb.py
@@ -0,0 +1,19 @@
+"""This specifies a django management command to populate the db with fake data
+"""
+
+from django.core.management.base import BaseCommand, CommandError
+
+from commgestion import settings
+from web import populate
+
+
+class Command(BaseCommand):
+    help = "Clears existing data and populates the local db with fake data"
+
+    def handle(self, *args, **options):
+        if not settings.DEBUG:
+            raise CommandError("Not overwriting production data! Not in a debug config!")
+
+        self.stdout.write("Beginning database population...")
+        populate.clear_and_add_defaults()
+        self.stdout.write(self.style.SUCCESS("Population complete!"))
