db5eead5a2d7301c1e378843ece5443d89fd40ad
Matt Johnson <matt9j@cs.washington.edu>
2020-06-11 13:54:14 -0700

    [populate] Clean up code style and unused imports

--- web/populate.py
+++ web/populate.py
@@ -1,18 +1,21 @@
+import random
+
 from datetime import timedelta
 
 from django.contrib.auth.models import User
-from web.models import HostUsage, UserDefinedHost, HostMapping, Subscriber, SubscriberUsage
-import random
 from django.utils import timezone
-from datetime import date
-from datetime import time
-from datetime import datetime
+
+from web.models import (HostUsage,
+                        UserDefinedHost,
+                        HostMapping,
+                        Subscriber,
+                        SubscriberUsage
+                        )
 
 
-# from django.contrib.auth.models import User
-
-# add more applications
 def add_applications():
+    """Add more applications
+    """
     HostUsage.objects.all().delete()
     hosts = ['https://google.com/', 'https://facebook.com', 'https://whatsapp.com', 'https://santainesapp.mx']
     for host_name in hosts:
@@ -24,8 +27,9 @@ def add_applications():
         )
 
 
-#  add host mapping as well as user defined hosts
 def add_hostmappings():
+    """Add host mapping as well as user defined hosts
+    """
     UserDefinedHost.objects.all().delete()
     captured_fb = list(['fb.cdn.com', 'dragon.cdn.com'])
     captured_google = list(['google.com', 'googlehost1.com', 'googlehost2.com', 'googlehost3'])
@@ -52,9 +56,7 @@ def add_hostmappings():
 
 
 def add_subscribers(subscriber_total=10):
-    """
-    #add subscribers, which requires fake "User" info like email phone imsi
-    def add_subscribers():
+    """Add subscribers, which requires fake "User" info like email phone imsi
     """
     Subscriber.objects.all().delete()
     User.objects.filter(is_superuser=False).all().delete()
@@ -71,9 +73,13 @@ def add_subscribers(subscriber_total=10):
         email_value = email_format.format(i)
         password_value = password_format.format(i)
         guti_value = guti_format.format(i)
-        User.objects.create_user(username=imsi_value, email=email_value, password=password_value)
+        User.objects.create_user(
+            username=imsi_value,
+            email=email_value,
+            password=password_value
+        )
+
         created_user = User.objects.get(username=imsi_value)
-        INSERT_TIMESTAMP = timezone.now()
 
         Subscriber.objects.create(
             user=created_user,
@@ -84,19 +90,11 @@ def add_subscribers(subscriber_total=10):
             is_local=True if i % 2 == 0 else False,
             role=Subscriber.Role.USER_ROLE,
             connectivity_status=Subscriber.ConnectionStatus.ONLINE,
-            last_time_online=INSERT_TIMESTAMP,
+            last_time_online=timezone.now(),
             rate_limit_kbps=100,
         )
 
         created_subscriber = Subscriber.objects.get(phonenumber=imsi_value)
-        # for j in range(30):
-        #     SubscriberUsage.objects.create(user=created_subscriber, throughput=50 * random.random(), timestamp=timezone.now())
-
-        #print( datetime.now() + timedelta(days=1))
-        #Subtract 60 seconds.
-        #print( datetime.now() - timedelta(seconds=60))
-        #Add 2 years.
-        #print( datetime.now() + timedelta(days=730))
 
         for j in range(30):
             SubscriberUsage.objects.create(
@@ -105,10 +103,7 @@ def add_subscribers(subscriber_total=10):
                 up_kbytes=10 * random.random(),
                 timestamp=timezone.now(),
             )
-            #having trouble printing anything out
-            #where is the bug
-            print(datetime.now() + timedelta(days=1))
-            print(datetime.now() + timedelta(weeks=1))
+
 
 def get_subscribers():
     total = Subscriber.objects.all()
