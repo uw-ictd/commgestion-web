266029dc6976548e2effd36cd5e9b187ca81dec3
Matt Johnson <matt9j@cs.washington.edu>
2020-06-24 15:40:31 -0700

    [web] Update add user form to use parsley compatible regex
    This commit updates the regex used to validate the IMSI field to not
    include a length specifier, and rely on the field max length parameter
    instead. This allows the field to be populated directly without a
    special condition in the template.

--- web/forms.py
+++ web/forms.py
@@ -54,14 +54,25 @@ class ModalForm(forms.Form):
     first_name = forms.CharField(label='First Name')
     last_name = forms.CharField(label='Last Name')
     email = forms.EmailField(label='Email Address')
-    imsi = forms.RegexField(label="IMSI", help_text='10-digit number on SIM card', regex='[0-9]{10}', min_length=10, max_length=10, error_messages = {
-         'required':"Please Enter 10 digit number"
-    }, strip=True)
+    imsi = forms.RegexField(
+        label="IMSI",
+        help_text='10-digit number on SIM card',
+        regex='[0-9]*',
+        min_length=10,
+        max_length=10,
+        error_messages={'required': "Please Enter 10 digit number"},
+        strip=True,
+    )
     role = forms.ChoiceField(choices=ROLES)
     connection_status = forms.ChoiceField(choices=CONN_STATUS, label='Connection Status')
     rate_limit = forms.DecimalField()
+    phone = forms.RegexField(
+        label="Phone Number",
+        regex=r'[0-9]*',
+        min_length=4,
+        strip=True,
+    )
     # guti = forms.RegexField(label="GUTI", help_text='5 digits ???? following IMSI value', regex=r'^[0-9]{5}$', strip=True)
-    # phone = forms.RegexField(label="Phone Number", regex=r'^[0-9]{5}$', strip=True)
     # resident_status = forms.ChoiceField(choices=LOCAL, label='Local Resident?')
 
 # delete form
